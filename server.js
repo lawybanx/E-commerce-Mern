1774f676b48f5f9d8a93794d64ec0dccfaf54444
Lawybanx <awwalbankole@gmail.com>
2021-12-02 17:42:35 +0100

    set up backend backbone

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+// Connect MongoDB at default port 27017.
+connectDB();
+
+// Express Init
+const app = express();
+
+// Body-parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Morgan Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+
+// Static Build Folder
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// PORT
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.blue
+      .bold
+  );
+});
