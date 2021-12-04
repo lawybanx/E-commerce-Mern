f0054c6a8967a0599a1b7b970383a0a18558f832
Lawybanx <awwalbankole@gmail.com>
2021-12-04 22:06:06 +0100

    update models with user and order

---
+++ backend/models/User.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+// Declare the Schema of the Mongo model
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Export the model
+export default model('User', userSchema);
