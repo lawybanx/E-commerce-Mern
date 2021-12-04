ca30d4f23fdeedc81dd9ab6b85fe0040e5edc0e1
Lawybanx <awwalbankole@gmail.com>
2021-12-04 16:13:46 +0100

    refactor product details screens

---
+++ frontend/src/components/ProductDetails.js
@@ -0,0 +1,73 @@
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Rating from '../components/Rating';
+
+const ProductDetails = ({
+  product: {
+    name,
+    image,
+    price,
+    rating,
+    numReviews,
+    description,
+    countInStock,
+  },
+}) => {
+  return (
+    <>
+      <Link to='/'>
+        <Button className='my-3' variant='light'>
+          Go back
+        </Button>
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={image} alt={name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating value={rating} text={`${numReviews} reviews`} />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${price}</ListGroup.Item>
+            <ListGroup.Item>Description: {description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>{countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={countInStock === 0}
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductDetails;
