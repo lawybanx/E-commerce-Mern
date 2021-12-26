f52f42bce5f3384d576ec48a49ecefd60a8e0c76
Awwal Bankole <awwalbankole@gmail.com>
2021-12-26 20:16:20 +0100

    refactor controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,11 @@
 import asyncHandler from 'express-async-handler';
 import { validationResult } from 'express-validator';
 import generateToken from '../utils/generateToken.js';
-import bcrypt from 'bcryptjs';
 
 // Bring in Model
 import User from '../models/User.js';
 
-//  @route  GET api/auth/users
+//  @route  GET api/users
 //  @desc   Get All Users
 //  @access Private
 
@@ -16,11 +15,11 @@ export const getUsers = asyncHandler(async (req, res, next) => {
   return res.status(200).json(users);
 });
 
-//  @route  POST api/auth/register
+//  @route  POST api/users
 //  @desc   Register new user
 //  @access Public
 
-export const registerUser = asyncHandler(async (req, res) => {
+export const registerUser = asyncHandler(async (req, res, next) => {
   // Get Errors
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -31,27 +30,29 @@ export const registerUser = asyncHandler(async (req, res) => {
 
   // Check for existing user
   const userExists = await User.findOne({ email });
-  if (userExists) return res.status(400).json({ msg: 'User already exists' });
-
-  // Create Salt & Hash password
-  const salt = await bcrypt.genSalt(10);
-  if (!salt) throw Error('Something went wrong with bcrypt');
-
-  const hash = await bcrypt.hash(password, salt);
-  if (!hash) throw Error('Something went wrong hashing the password');
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
 
   const user = await User.create({
     name,
     email,
-    password: hash,
+    password,
   });
 
-  if (!user) throw Error('Something went wrong saving the user');
+  if (!user) throw new Error('Something went wrong saving the user');
 
-  res.status(201).json({ token: generateToken(user._id) });
+  res.status(201).json({
+    _id: user.id,
+    name: user.name,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    token: generateToken(user._id),
+  });
 });
 
-//  @route  POST api/auth/login
+//  @route  POST api/users/login
 //  @desc   Login user
 //  @access Public
 
@@ -66,22 +67,21 @@ export const loginUser = asyncHandler(async (req, res) => {
 
   // Check for existing user
   const user = await User.findOne({ email });
-  if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
 
-  // Validating password
-  const isMatch = await bcrypt.compare(password, user.password);
-  if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
-
-  res.status(200).json({
-    _id: user.id,
-    name: user.name,
-    email: user.email,
-    isAdmin: user.isAdmin,
-    token: generateToken(user._id),
-  });
+  if (user && (await user.matchPassword(password))) {
+    res.status(200).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  }
+  res.status(401);
+  throw new Error('Invalid credentials');
 });
 
-//  @route  GET api/auth/profile
+//  @route  GET api/Users/profile
 //  @desc   Get user profile data
 //  @access Private
 
