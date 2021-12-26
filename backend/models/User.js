774b02cf9486abc476d711ce94e4b7f7ea90805f
Awwal Bankole <awwalbankole@gmail.com>
2021-12-26 20:16:04 +0100

    encrypt password from model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const { Schema, model } = mongoose;
 // Declare the Schema of the Mongo model
@@ -27,6 +28,19 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 // Export the model
 export default model('User', userSchema);
