c9af9cb8aa3e753d1c757f2c8636068162eda35e
Awwal Bankole <awwalbankole@gmail.com>
2021-12-27 14:35:48 +0100

    add user reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { cart } from './cart';
+import { userLogin } from './user';
 import { productList, productDetails } from './product';
 
 export default combineReducers({
   productList,
   productDetails,
   cart,
+  userLogin,
 });
