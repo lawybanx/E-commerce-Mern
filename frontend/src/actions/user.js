8113ae399d04e4c7f01703c781c9848b654f5eb5
Awwal Bankole <awwalbankole@gmail.com>
2021-12-30 20:56:59 +0100

    register action and reducer

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -4,6 +4,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from '../constants/userConstants';
 
 // Login User
@@ -22,6 +25,30 @@ export const loginUser = formData => async dispatch => {
   } catch (err) {
     dispatch({
       type: USER_LOGIN_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.response.data.errors,
+    });
+  }
+};
+
+// Register User
+export const registerUser = formData => async dispatch => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+
+    const { data } = await axios.post('/api/users', formData);
+
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (err) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
       payload:
         err.response && err.response.data.message
           ? err.response.data.message
--- frontend/src/reducers/user.js
+++ frontend/src/reducers/user.js
@@ -3,17 +3,23 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
 } from '../constants/userConstants';
 
 export const userLogin = (state = {}, { type, payload }) => {
   switch (type) {
     case USER_LOGIN_REQUEST:
+    case USER_REGISTER_REQUEST:
       return { loading: true };
 
     case USER_LOGIN_SUCCESS:
+    case USER_REGISTER_SUCCESS:
       return { loading: false, userInfo: payload };
 
     case USER_LOGIN_FAIL:
+    case USER_REGISTER_FAIL:
       return { loading: false, error: payload };
 
     case USER_LOGOUT:
